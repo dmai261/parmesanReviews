79eaae597dbf2c88f1b1568a557a3dbf985587fa
Alexander Vincent-Hill <alexanderjvincenthill@gmail.com>
2018-09-06 16:23:06 -0700

    Serve static react with express, GET data onmount

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
+import $ from 'jquery';
 // import style from '../styles/main.css'; // TBD get modular css working
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.serverUrl = 'http://localhost:1337';
+    this.state = {
+      reviews: [],
+      currentProductId: 37,
+    };
+  }
+
+  componentDidMount() {
+    // TBD GET request to server for review data
+    // TBD refactor ajax request to fetch
+    const settings = {
+      async: true,
+      crossDomain: true,
+      url: `${this.serverUrl}/reviews/${this.state.currentProductId}`,
+      method: 'GET',
+      headers: {
+        content-type: 'application/json',
+        cache-control: 'no-cache',
+      },
+    };
+
+    $.ajax(settings).done((data) => {
+      console.log('successful GET!');
+      this.setState({ reviews: data });
+    });
   }
 
   render() {
+    const { state } = this;
     return (
-      <div>Hello app!</div>
-    )
+      <div>{state.reviews}</div>
+    );
   }
 }
 
