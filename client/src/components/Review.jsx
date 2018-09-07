9468376a0f426ec1b72a6bf63d07c8d088f63dd6
Alexander Vincent-Hill <alexanderjvincenthill@gmail.com>
2018-09-06 19:34:05 -0700

    Mock out review content

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -7,14 +7,29 @@ class Review extends React.Component {
   }
 
   render() {
-    const state = this.props.getState();
-    const { reviews } = state;
+    const { review } = this.props;
     return (
       <React.Fragment>
-        <h3>{JSON.stringify(reviews[0])}</h3>
+        <br></br>
+        <div>%PIC% {review.username}</div>
+        <br></br>
+        <div>***** <b>{review.title}</b></div>
+        <div>{review.timestamp}</div>
+        <div>Verified? {review.verifiedPurchase}</div>
+        <div>{review.text}</div>
+        <br></br>
+        <div>{review.numHelpful} people found this helpful</div>
+        <br></br>
+        <div><button>Helpful</button> Comment Report Abuse</div>
+        <br></br>
       </React.Fragment>
     );
   }
 }
 
 export default Review;
+
+/*
+example data
+{"_id":"5b918722d4a71c1897abb02f","productId":37,"reviewId":368,"username":"Parker_Nader66","stars":1,"title":"est dolore ducimus","text":"Vel molestias consequatur voluptatum vero labore dicta repellendus aut corporis. Et cupiditate minus nam. Perspiciatis dolorem omnis provident.","timestamp":"2018-02-01T17:26:28.993Z","numHelpful":965,"verifiedPurchase":true,"imageUrl":"http://lorempixel.com/640/480","__v":0}
+*/
--- client/src/components/TopReviews.jsx
+++ client/src/components/TopReviews.jsx
@@ -4,14 +4,22 @@ import Review from './Review.jsx';
 class TopReviews extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    // };
   }
+
   render() {
+    const state = this.props.getState();
+    const { reviews } = state;
+    const reviewElements = reviews.map((review, index) => {
+      if (index < 5) {
+        return <Review key={index} review={review} />;
+      }
+    });
     return (
       <React.Fragment>
         <h3>Top customer reviews</h3>
-        <Review getState={this.props.getState} />
+        {reviewElements}
+        <p>See all 10 reviews > </p>
+        <button>Write a customer review</button>
       </React.Fragment>
     );
   }
