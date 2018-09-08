f9e297aa913efa21a3db9def0e0d786d1b2ea5a1
Alexander Vincent-Hill <alexanderjvincenthill@gmail.com>
2018-09-07 20:10:24 -0700

    Style client

--- client/src/components/Mentions.jsx
+++ client/src/components/Mentions.jsx
@@ -4,17 +4,26 @@ import styles from '../styles/Mentions.css';
 class Mentions extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    // };
   }
+
   render() {
     const state = this.props.getState();
     const { reviews } = state;
     return (
       <React.Fragment>
         <h4>Read reviews that mention</h4>
-        <p>MENTIONS HERE     MENTIONS HERE     MENTIONS HERE      MENTIONS HERE</p>
-        <p>        MENTIONS HERE     MENTIONS HERE      MENTIONS HERE</p>
+        <div className={styles.mentionContainer}>
+          <div className={styles.mentionItem}>mention</div>
+          <div className={styles.mentionItem}>mentionmention</div>
+          <div className={styles.mentionItem}>mention</div>
+          <div className={styles.mentionItem}>mention</div>
+          <div className={styles.mentionItem}>mention</div>
+          <div className={styles.mentionItem}>mentionmention</div>
+          <div className={styles.mentionItem}>meon</div>
+          <div className={styles.mentionItem}>mentmention</div>
+          <div className={styles.mentionItem}>mention</div>
+          <div className={styles.mentionItem}>mtion</div>
+        </div>
         <p className={styles.blue}>+ See more</p>
       </React.Fragment>
     );
