7de58ce762d39862c1eb04fd61ccee0a71a920dd
Alexander Vincent-Hill <alexanderjvincenthill@gmail.com>
2018-09-12 18:22:18 -0700

    Test randomness of getRandomInt

--- test/test.js
+++ test/test.js
@@ -1,4 +1,24 @@
+const helpers = require('../server/helpers');
+
 test('expect 1 to equal 1', () => {
   const sum = 1;
   expect(sum).toBe(1);
+});
+
+
+test('expect getRandomInt to be a function', () => {
+  expect(typeof helpers.getRandomInt).toBe('function');
+});
+
+test('expect getRandomInt to generate random data', () => {
+  let result = 0;
+
+  for (let i = 0; i < 10000; i++) {
+    result += helpers.getRandomInt(10);
+  }
+
+  result = result / 10000;
+
+  expect(result).toBeGreaterThan(4);
+  expect(result).toBeLessThan(6);
 });
